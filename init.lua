f1f9b6a9dfeebbe8464787b29f2ebea4347aaecb
chengzh <loveandloss@hotmail.com>
2025-01-31 16:27:30 +0800

    Set the color column at column 80 for C++ related files

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Set the color column at column 80 for C++ related files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cpp", "h", "hpp", "cc" },
+  callback = function()
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
