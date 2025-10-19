d475b7e99fefdda0370f40975919cf929cabbc9b
chengzh <loveandloss@hotmail.com>
2025-10-19 13:42:39 +0000

    Add shortcut key <leader>th to control inlay hints

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 切换 LSP inlay hints 显示状态
+vim.keymap.set("n", "<leader>th", function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+  vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
+  print("Inlay hints " .. (not enabled and "enabled" or "disabled"))
+end, { desc = "Toggle Inlay Hints" })
+
